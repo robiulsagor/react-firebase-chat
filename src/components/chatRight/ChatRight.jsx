31532b471a4b0a5816a93a582a1a37df5de409db
robiulsagor <robiul100.me@gmail.com>
2024-08-16 21:08:14 +0600

    chat right side done design

--- src/components/chatRight/ChatRight.jsx
+++ src/components/chatRight/ChatRight.jsx
@@ -1,7 +1,69 @@
 import "./chatRight.css";
 
 const ChatRight = () => {
-  return <div className="chat-right">ChatRight</div>;
+  return (
+    <div className="chat-right">
+      <div className="profile-info">
+        <img
+          className="profile-img"
+          src="src/assets/profile_martin.png"
+          alt=""
+        />
+        <h3>Robiul Islam Sagar</h3>
+        <span>Hey, I&apos;m using chat app</span>
+      </div>
+
+      <hr />
+
+      <div className="media-container">
+        <h2 className="media-text">Media</h2>
+        <div className="media-imgs">
+          <img src="src/assets/pic1.png" alt="" />
+          <img src="src/assets/pic1.png" alt="" />
+          <img src="src/assets/pic1.png" alt="" />
+          <img src="src/assets/pic1.png" alt="" />
+          <img src="src/assets/pic1.png" alt="" />
+          <img src="src/assets/pic1.png" alt="" />
+          <img src="src/assets/pic1.png" alt="" />
+          <img src="src/assets/pic1.png" alt="" />
+          <img src="src/assets/pic1.png" alt="" />
+          <img src="src/assets/pic1.png" alt="" />
+          <img src="src/assets/pic1.png" alt="" />
+          <img src="src/assets/pic1.png" alt="" />
+          <img src="src/assets/pic1.png" alt="" />
+          <img src="src/assets/pic1.png" alt="" />
+          <img src="src/assets/pic1.png" alt="" />
+          <img src="src/assets/pic1.png" alt="" />
+          <img src="src/assets/pic1.png" alt="" />
+          <img src="src/assets/pic1.png" alt="" />
+          <img src="src/assets/pic1.png" alt="" />
+          <img src="src/assets/pic1.png" alt="" />
+          <img src="src/assets/pic1.png" alt="" />
+          <img src="src/assets/pic1.png" alt="" />
+          <img src="src/assets/pic1.png" alt="" />
+          <img src="src/assets/pic1.png" alt="" />
+          <img src="src/assets/pic1.png" alt="" />
+          <img src="src/assets/pic1.png" alt="" />
+          <img src="src/assets/pic1.png" alt="" />
+          <img src="src/assets/pic1.png" alt="" />
+          <img src="src/assets/pic1.png" alt="" />
+          <img src="src/assets/pic1.png" alt="" />
+          <img src="src/assets/pic1.png" alt="" />
+          <img src="src/assets/pic1.png" alt="" />
+          <img src="src/assets/pic1.png" alt="" />
+          <img src="src/assets/pic1.png" alt="" />
+          <img src="src/assets/pic1.png" alt="" />
+          <img src="src/assets/pic1.png" alt="" />
+          <img src="src/assets/pic1.png" alt="" />
+          <img src="src/assets/pic1.png" alt="" />
+          <img src="src/assets/pic1.png" alt="" />
+          <img src="src/assets/pic1.png" alt="" />
+          <img src="src/assets/pic1.png" alt="" />
+          <img src="src/assets/pic1.png" alt="" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ChatRight;
