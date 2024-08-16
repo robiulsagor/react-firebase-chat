666e06ee058eae7f32adea33b969fb0468e78074
robiulsagor <robiul100.me@gmail.com>
2024-08-17 00:39:19 +0600

    update profile page design done

--- src/pages/profileUpdate/ProfileUpdate.jsx
+++ src/pages/profileUpdate/ProfileUpdate.jsx
@@ -1,7 +1,49 @@
+import { useState } from "react";
 import "./profile.css";
 
 const ProfileUpdate = () => {
-  return <div>ProfileUpdate</div>;
+  const [img, setImg] = useState(null);
+
+  return (
+    <div className="p-wrapper">
+      <div className="p-container">
+        <div className="p-main">
+          <form className="p-left">
+            <h3>Update Profile</h3>
+
+            <input
+              type="file"
+              name=""
+              id="file"
+              hidden
+              onChange={(e) => setImg(e.target.files[0])}
+            />
+            <label htmlFor="file">
+              <img
+                src={
+                  img ? URL.createObjectURL(img) : "src/assets/avatar_icon.png"
+                }
+                alt=""
+              />
+              Upload Profile Picture
+            </label>
+
+            <input type="text" name="" id="" placeholder="Name" />
+            <textarea name="" id="" placeholder="Bio" rows={3}></textarea>
+            <button type="submit">Save</button>
+          </form>
+          <div className="p-right">
+            <img
+              src={
+                img ? URL.createObjectURL(img) : "src/assets/avatar_icon.png"
+              }
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProfileUpdate;
